ccd713f1d52728369f5e74d7fab8d4a28649408e
lucieDLE <dolelucie0@gmail.com>
2022-08-30 12:14:41 -0500

    remove comment

--- src/m/average_population.m
+++ src/m/average_population.m
@@ -1,30 +1,25 @@
 % function average_population(INPUT_DIR, OUTPUT_DIR)
 % average_population_hrf - compute the average of the HRF parameters accross a population
-
-% INPUT_DIR='../../../rsHRF_project/ADNI_23';
-% OUTPUT_DIR='../../../rsHRF_project/output/average/population';
-
-% files =  dir([INPUT_DIR '/*/*/func2stand/func2stand_Height_no_ants.nii.gz']);
-% hrf_files =  dir([INPUT_DIR '/*/*/rsfmri/Deconv_Detrend_4DVolume_hrf.nii']);
+% files =  dir([INPUT_DIR '/*/ants_func2stand/func2stand_Olrm_Height.nii'])
 
 
 length(files)
 
 idx = 1;
 
-% Height = load_nii([files(idx).folder '/func2stand_Height_no_ants.nii.gz']);
-% DipHeight = load_nii([files(idx).folder '/func2stand_DipHeight_no_ants.nii.gz']);
-% TroughHeight = load_nii([files(idx).folder '/func2stand_TroughHeight.nii.gz']);
+Height = load_nii([files(idx).folder '/func2stand_Olrm_Height.nii']);
+DipHeight = load_nii([files(idx).folder '/func2stand_DipHeight.nii']);
+TroughHeight = load_nii([files(idx).folder '/func2stand_TroughHeight.nii']);
 
-% PeakIntegral= load_nii([files(idx).folder '/func2stand_PeakIntegral_v2.nii.gz']);
-% DipIntegral = load_nii([files(idx).folder '/func2stand_DipIntegral_v2.nii.gz']);
-% TroughIntegral = load_nii([files(idx).folder '/func2stand_TroughIntegral.nii.gz']);
+PeakIntegral= load_nii([files(idx).folder '/func2stand_PeakIntegral.nii']);
+DipIntegral = load_nii([files(idx).folder '/func2stand_DipIntegral.nii']);
+TroughIntegral = load_nii([files(idx).folder '/func2stand_TroughIntegral.nii']);
 
-% TTP = load_nii([files(idx).folder '/func2stand_Time2peak_no_ants.nii.gz']);
-% TTD = load_nii([files(idx).folder '/func2stand_Time2dip_no_ants.nii.gz']);
-% TTT = load_nii([files(idx).folder '/func2stand_Time2trough.nii.gz']);
+TTP = load_nii([files(idx).folder '/func2stand_Olrm_Time2peak.nii']);
+TTD = load_nii([files(idx).folder '/func2stand_Time2dip.nii']);
+TTT = load_nii([files(idx).folder '/func2stand_Time2trough.nii']);
 
-% FWHM = load_nii([files(idx).folder '/func2stand_FWHM_no_ants.nii.gz']);
+FWHM = load_nii([files(idx).folder '/func2stand_Olrm_FWHM.nii']);
 
 j=1;
 
@@ -32,70 +27,68 @@ for i=idx+1:length(files)
     i;
     folder = files(i).folder;
     try
-        vol = load_nii([hrf_files(i).folder '/' hrf_files(i).name]);
-        sz = size(vol.img);
-        if sz(4) == 11
-            j = j+ 1
+        % vol = load_nii([hrf_files(i).folder '/' hrf_files(i).name]);
+        % sz = size(vol.img);
+        % if sz(4) == 17
+        j = j+ 1
+    
+        tmp = load_nii([files(i).folder '/func2stand_Olrm_Height.nii']);
+        Height.img = Height.img + tmp.img;
+        
+        tmp = load_nii([files(i).folder '/func2stand_DipHeight.nii']);
+        DipHeight.img = DipHeight.img + tmp.img;
+        
+        tmp = load_nii([files(i).folder '/func2stand_TroughHeight.nii']);
+        TroughHeight.img = TroughHeight.img + tmp.img;
 
-            % tmp = load_nii([files(i).folder '/func2stand_Height_no_ants.nii.gz']);
-            % Height.img = Height.img + tmp.img;
-            
-            % tmp = load_nii([files(i).folder '/func2stand_DipHeight_no_ants.nii.gz']);
-            % DipHeight.img = DipHeight.img + tmp.img;
-            
-            % tmp = load_nii([files(i).folder '/func2stand_TroughHeight.nii.gz']);
-            % TroughHeight.img = TroughHeight.img + tmp.img;
+        tmp = load_nii([files(i).folder '/func2stand_PeakIntegral.nii']);
+        PeakIntegral.img= PeakIntegral.img + tmp.img;
+        
+        tmp = load_nii([files(i).folder '/func2stand_DipIntegral.nii']);
+        DipIntegral.img = DipIntegral.img + tmp.img;
+        
+        tmp = load_nii([files(i).folder '/func2stand_TroughIntegral.nii']);
+        TroughIntegral.img = TroughIntegral.img + tmp.img;
 
-            % tmp = load_nii([files(i).folder '/func2stand_PeakIntegral_v2.nii.gz']);
-            % PeakIntegral.img= PeakIntegral.img + tmp.img;
-            
-            % tmp = load_nii([files(i).folder '/func2stand_DipIntegral_v2.nii.gz']);
-            % DipIntegral.img = DipIntegral.img + tmp.img;
-            
-            % tmp = load_nii([files(i).folder '/func2stand_TroughIntegral.nii.gz']);
-            % TroughIntegral.img = TroughIntegral.img + tmp.img;
+        tmp = load_nii([files(i).folder '/func2stand_Olrm_Time2peak.nii']);
+        TTP.img = TTP.img + tmp.img;
+        
+        tmp = load_nii([files(i).folder '/func2stand_Time2dip.nii']);
+        TTD.img = TTD.img + tmp.img;
+        
+        tmp = load_nii([files(i).folder '/func2stand_Time2trough.nii']);
+        TTT.img = TTT.img + tmp.img;
 
-            % tmp = load_nii([files(i).folder '/func2stand_Time2peak_no_ants.nii.gz']);
-            % TTP.img = TTP.img + tmp.img;
-            
-            % tmp = load_nii([files(i).folder '/func2stand_Time2dip_no_ants.nii.gz']);
-            % TTD.img = TTD.img + tmp.img;
-            
-            % tmp = load_nii([files(i).folder '/func2stand_Time2trough.nii.gz']);
-            % TTT.img = TTT.img + tmp.img;
-
-            % tmp = load_nii([files(i).folder '/func2stand_FWHM_no_ants.nii.gz']);
-            % FWHM.img = FWHM.img + tmp.img;
-
-        end
+        tmp = load_nii([files(i).folder '/func2stand_Olrm_FWHM.nii']);
+        FWHM.img = FWHM.img + tmp.img;
+        % end
     catch
     end
 end
 j
 
-% Height.img = Height.img ./j;
-% DipHeight.img = DipHeight.img ./j;
-% TroughHeight.img = TroughHeight.img ./j;
-% PeakIntegral.img = PeakIntegral.img ./j;
-% DipIntegral.img = DipIntegral.img ./j;
-% TroughIntegral.img = TroughIntegral.img ./j;
-% TTP.img = TTP.img ./j;
-% TTD.img = TTD.img ./j;
-% TTT.img = TTT.img ./j;
-% FWHM.img = FWHM.img ./j;
+Height.img = Height.img ./j;
+DipHeight.img = DipHeight.img ./j;
+TroughHeight.img = TroughHeight.img ./j;
+PeakIntegral.img = PeakIntegral.img ./j;
+DipIntegral.img = DipIntegral.img ./j;
+TroughIntegral.img = TroughIntegral.img ./j;
+TTP.img = TTP.img ./j;
+TTD.img = TTD.img ./j;
+TTT.img = TTT.img ./j;
+FWHM.img = FWHM.img ./j;
 
-% param = '/HRF_41/'
-% save_nii(Height, [ OUTPUT_DIR param 'Height.nii'])
-% save_nii(DipHeight, [ OUTPUT_DIR param 'DipHeight.nii'])
-% save_nii(TroughHeight, [ OUTPUT_DIR param 'TroughHeight.nii'])
+param = ''
+save_nii(Height, [ OUTPUT_DIR param 'Height.nii'])
+save_nii(DipHeight, [ OUTPUT_DIR param 'DipHeight.nii'])
+save_nii(TroughHeight, [ OUTPUT_DIR param 'TroughHeight.nii'])
 
-% save_nii(PeakIntegral, [ OUTPUT_DIR param 'PeakIntegral.nii'])
-% save_nii(DipIntegral, [ OUTPUT_DIR param 'DipIntegral.nii'])
-% save_nii(TroughIntegral, [ OUTPUT_DIR param 'TroughIntegral.nii'])
+save_nii(PeakIntegral, [ OUTPUT_DIR param 'PeakIntegral.nii'])
+save_nii(DipIntegral, [ OUTPUT_DIR param 'DipIntegral.nii'])
+save_nii(TroughIntegral, [ OUTPUT_DIR param 'TroughIntegral.nii'])
 
-% save_nii(TTP, [ OUTPUT_DIR param 'TTP.nii'])
-% save_nii(TTD, [ OUTPUT_DIR param 'TTD.nii'])
-% save_nii(TTP, [ OUTPUT_DIR param 'TTP.nii'])
+save_nii(TTP, [ OUTPUT_DIR param 'TTP.nii'])
+save_nii(TTD, [ OUTPUT_DIR param 'TTD.nii'])
+save_nii(TTT, [ OUTPUT_DIR param 'TTT.nii'])
 
-% save_nii(FWHM, [ OUTPUT_DIR param 'FWHM.nii'])    
-% % save_nii(nii,[folder 'TroughIntegral.nii'])
+save_nii(FWHM, [ OUTPUT_DIR param 'FWHM.nii'])    
--- src/m/plot_avg_hrf_WM_GM.m
+++ src/m/plot_avg_hrf_WM_GM.m
@@ -1,33 +1,35 @@
-INPUT_DIR='../../../rsHRF_project/ADNI_23';
-ATLAS_DIR='../../../rsHRF_project/atlases';
+% INPUT_DIR='../../../rsHRF_project/ADNI_23';
+INPUT_DIR='/nfs2/rsHRF/BLSA';
+
+ATLAS_DIR='../../../rsHRF_project/atlases/MNI';
 param = 'hrf';
 
-OUT_DIR = '../../../rsHRF_project/hrf_test';
+OUT_DIR = '../../../rsHRF_project/';
 
 %% compute HRF average population 
-% files =  dir([INPUT_DIR '/*/*/rsfmri/Deconv_Canonical_Detrend_4DVolume_hrf.nii']);
+files =  dir([INPUT_DIR '/*/Deconv/Deconv_Detrend_4DVolume_hrf.nii']);
 
-% length(files)
-% idx = 1;
-% Param = load_nii([files(idx).folder '/' files(1).name]);
-% j=1;
-% for i=idx+1:length(files)
-%     i
-%     folder = files(i).folder;
-%     try
-%         vol = load_nii([folder '/' files(i).name]);
-%         sz = size(vol.img);
-%         if sz(4) == 11
-%             j = j+ 1;
-%             Param.img = Param.img + vol.img;
-%         end
-%     catch
-%         error_message = ['error in ' vol];
-%         disp(error_message)
-%     end
-% end
-% message = ['number of volumes used:' num2str(j)];
-% disp(message)
+length(files)
+idx = 1;
+Param = load_nii([files(idx).folder '/' files(1).name]);
+j=1;
+for i=idx+1:length(files)
+    i
+    folder = files(i).folder;
+    try
+        vol = load_nii([folder '/' files(i).name]);
+        sz = size(vol.img);
+        if sz(4) == 17
+            j = j+ 1;
+            Param.img = Param.img + vol.img;
+        end
+    catch
+        error_message = ['error in ' vol];
+        disp(error_message)
+    end
+end
+message = ['number of volumes used:' num2str(j)];
+disp(message)
 
 % Param.img = Param.img ./j;
 % avg_hrf_file = ['../../../rsHRF_project/output/average_population/' param '_canonical.nii'];
@@ -37,34 +39,34 @@ OUT_DIR = '../../../rsHRF_project/hrf_test';
 
 % %% regrid img to match template size
 
-% template = [ATLAS_DIR '/mni_icbm152_nlin_asym_09c/mni_icbm152_t1_tal_nlin_asym_09c.nii'];
+template = [ATLAS_DIR '/mni_icbm152_nlin_asym_09c/mni_icbm152_t1_tal_nlin_asym_09c.nii'];
 
 % in_one_sub = [INPUT_DIR '/VUIISXNAT04_S16966/VUIISXNAT04_E232335/rsfmri/Deconv_canonical_Detrend_4DVolume_hrf.nii'];
 % out_one_sub = [OUT_DIR '/one_sub_sized.nii'];
 
-% in_avg_sub = avg_hrf_file;
-% out_avg_sub = [OUT_DIR '/avg_sub_sized.nii'];
+in_avg_sub = avg_hrf_file;
+out_avg_sub = [OUT_DIR '/avg_sub_sized_blsa.nii'];
 
 
 % system(['mrgrid ' in_one_sub ' regrid -size 193,229,193 ' out_one_sub])
-% system(['mrgrid ' in_avg_sub ' regrid -size 193,229,193 ' out_avg_sub])
+system(['mrgrid ' in_avg_sub ' regrid -size 193,229,193 ' out_avg_sub])
 
 
 %% write image to same voxel size
 
 % one_sub_vox = [OUT_DIR '/one_sub_voxed.nii'];
-% avg_sub_vox = [OUT_DIR '/avg_sub_voxed.nii'];
+avg_sub_vox = [OUT_DIR '/avg_sub_voxed_blsa.nii'];
 
 % tmp = niftiread(out_one_sub);
 % niftiwrite(tmp, one_sub_vox);
 
-% tmp = niftiread(out_avg_sub);
-% niftiwrite(tmp, avg_sub_vox);
+tmp = niftiread(out_avg_sub);
+niftiwrite(tmp, avg_sub_vox);
 
-% %% compute and plot avg in WM/GM
-
-mask_gm =load_nii([ATLAS_DIR '/gm.nii']);
-mask_wm =load_nii([ATLAS_DIR '/wm.nii']);
+%% compute and plot avg in WM/GM
+MASK_DIR='../../../rsHRF_project/atlases';
+mask_gm =load_nii([MASK_DIR '/gm.nii']);
+mask_wm =load_nii([MASK_DIR '/wm.nii']);
 
 list_index_gm=find(mask_gm.img==1);
 list_index_wm=find(mask_wm.img==1);
@@ -72,18 +74,16 @@ list_index_wm=find(mask_wm.img==1);
 
 % name = ['one subject', 'mean population'];
 
-f = figure()
-TR = [3, 0.607];
+TR = [2, 2];
 
-img_11_gm = '../../../rsHRF_project/hrf_test/HRF_11/avg_sub_vox.nii';
-img_53_gm = '../../../rsHRF_project/hrf_test/HRF_53/avg_sub_vox.nii';
-str = [  img_11_gm ' ' img_53_gm ];
+% img_11_gm = '../../../rsHRF_project/avg_sub_voxed.nii';
+img_53_gm = '../../../rsHRF_project/avg_sub_voxed_blsa.nii';
+str = [ img_53_gm ];
 files_cell = split(str);
-color = [ [0 0.4470 0.7410], [0.6350 0.0780 0.1840], 'r', 'b' ];
 
-gm = zeros([2, 54]);
-wm = zeros([2, 54]); 
-x =  zeros([2, 54]);
+gm = zeros([1, 18]);
+wm = zeros([1, 18]); 
+x =  zeros([1, 18]);
 
 for i=1:length(files_cell)
 
@@ -117,24 +117,19 @@ for i=1:length(files_cell)
     % plot(x, gm, color(j)); plot(x, wm, color(j+1));
     %     j = j+2
 end
+f = figure();
 
 hold on;
-plot(x(1,1:11), gm(1,1:11),'Color', [0.8500 0.3250 0.0980], 'LineWidth', 1)
-plot(x(1,1:11), wm(1,1:11), 'Color', [0.9290 0.6940 0.1250], 'LineWidth', 1) 
-plot(x(2,:), gm(2,:), 'Color', [0 0.4470 0.7410], 'LineWidth', 1)
-plot(x(2,:), wm(2,:), 'Color', [0.3010 0.7450 0.9330], 'LineWidth', 1);
+plot(x(1,:), wm(1,:),'Color', [0.8500 0.3250 0.0980], 'LineWidth', 1)
+plot(x(1,:), gm(1,:), 'Color', [0.9290 0.6940 0.1250], 'LineWidth', 1) 
+% plot(x(2,:), wm(2,:), 'Color', [0 0.4470 0.7410], 'LineWidth', 2)
+% plot(x(2,:), wm(2,:), 'Color', [0.3010 0.7450 0.9330], 'LineWidth', 2);
 
-[~, hobj, ~, ~] = legend('GM TR=3s', 'WM TR=3s', 'GM TR=0.607s', 'WM TR=0.607s', fontsize=15) 
+[~, hobj, ~, ~] = legend('WM', 'GM', fontsize=10);
 hl = findobj(hobj,'type','line');
 set(hl,'LineWidth',2);
 
 xlabel('time (s)', fontsize=15)
 ylabel('Signal Amplitude', fontsize=15)
-axis([x(1), x(end), -0.5, 2.1])
-exportgraphics(f,'barchart.png','Resolution',300)
-
-% img_11_gm = load_nii('Documents/rsHRF_project/hrf_test/HRF_11') [0 0.4470 0.7410]
-% img_11_wm = load_nii('Documents/rsHRF_project/hrf_test/HRF_11') [0.6350 0.0780 0.1840]
-
-% img_53_gm = load_nii('Documents/rsHRF_project/hrf_test/HRF_53') 'r'
-% img_53_wm = load_nii('Documents/rsHRF_project/hrf_test/HRF_53') 'b'
+axis([x(1), x(end), -1, 2])
+exportgraphics(f,'blsa.png','Resolution',600)
